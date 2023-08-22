ddb576404c580ec177d119e73b3b7bb99fe7c788
Adil Aldamzharov <Sarzhigitov.Adil@gmail.com>
2023-08-22 17:06:57 +0600

    init commit

---
+++ src/main/java/org/asaheyt/security/module/SecurityModuleApplication.java
@@ -0,0 +1,14 @@
+package org.asaheyt.security.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Security module application.
+ */
+@SpringBootApplication
+public class SecurityModuleApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SecurityModuleApplication.class, args);
+  }
+}
